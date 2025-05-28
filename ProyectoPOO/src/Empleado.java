2c97389428bd073f5c94a800bde660a88c06e126
raxel <zS23014100@estudiantes.uv.mx>
2025-05-28 11:11:02 -0600

    Clase empleado

--- ProyectoPOO/src/Empleado.java
+++ ProyectoPOO/src/Empleado.java
@@ -1,2 +1,109 @@
-public class Empleado {
+import java.util.Date;
+
+public abstract class Empleado {
+    private String nombre;
+    private String correo;
+    private String tipo;
+    private int telefono;
+    private String horario;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private int idEmpleado;
+    private String contraseña;
+
+    // Constructor por defecto
+    public Empleado() {
+    }
+
+    // Constructor con parámetros
+    public Empleado(String nombre, String correo, String tipo, int telefono, String horario,
+                    Date fechaInicio, Date fechaFin, int idEmpleado, String contraseña) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.tipo = tipo;
+        this.telefono = telefono;
+        this.horario = horario;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.idEmpleado = idEmpleado;
+        this.contraseña = contraseña;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    // Método abstracto que deben implementar las subclases
+    public abstract void mostrarInformacion();
 }
+
